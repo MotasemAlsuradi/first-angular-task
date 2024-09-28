41ad284f973cbb77b0adece0d158cdd253fbe61b
MotasemAlsuradi <alsuradym@gmail.com>
2024-09-28 18:36:48 +0300

    Static Web App Changes

--- src/app/Components/Pipes/highlighted-input-text-pipe.pipe.ts
+++ src/app/Components/Pipes/highlighted-input-text-pipe.pipe.ts
@@ -6,11 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class HighlightedInputTextPipe implements PipeTransform {
 
-  transform(value: string, search: string): string {
-    if (!search)
-      return value;
-
-    const regex = new RegExp(`(${search})`, 'gi');
-    return value.replace(regex, `<span style='background-color:rgba(0, 255, 0, 0.5);'>$1</span>`);
+  transform(value: string): string {
+    return value.replace(value, `<span style='background-color:rgba(0, 255, 0, 0.5);'>${{value}}</span>`);
   }
 }
